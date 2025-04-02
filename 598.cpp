22bdf800e55c74156aee15a226a99602f14c1f00
Shunaze-Sherwin <haivuongbd269@gmail.com>
2025-04-02 08:59:10 +0000

    .

---
+++ 598.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define ull unsigned ll
+#define pa pair<int, int>
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#define gcd __gcd
+#define log __lg
+#define endl '\n'
+#define upper upper_bound
+#define lower lower_bound
+#define all(a) (a).begin(), (a).end()
+#define bit(i, mask) (mask & (1ll << (i)))
+#define reset(a, val) memset(a, val, sizeof(a))
+#define fu(i, a, b) for (int i = (a); i <= (b); ++i)
+#define fd(i, a, b) for (int i = (a); i >= (b); --i)
+#define int ll
+
+constexpr int MOD = 1e5 + 3;
+constexpr int mx = 5e5;
+constexpr int maxn = (1 << 20);
+constexpr int LOG = log(mx);
+constexpr ll inf = 1e18 + 15;
+
+void add(int &a, int b){a += b; if (a >= MOD) a -= MOD;}
+void sub(int &a, int b){a -= b; if (a < 0) a += MOD;}
+void mul(int &a, int b){a = 1ll * (a%MOD) * (b%MOD) % MOD;}
+
+static const int SIZE = 1 << 16;
+char Achar[SIZE];
+int cpos = SIZE, clen = SIZE;
+
+inline int inchar(){
+    if (cpos == clen){
+        clen = (int)fread(Achar, 1, SIZE, stdin);
+        if (clen == 0) return EOF;
+        cpos = 0;
+    }
+    return Achar[cpos++];
+}
+
+inline int Read_int(){
+    int x = 0;
+    char c = inchar();
+    int p = 1;
+    while (c != EOF && (c < '0' || c > '9') && c != '-') c = inchar();
+    if (c == '-') p = -1, c = inchar();
+    while (c != EOF && (c >= '0' && c <= '9')) x = x*10 + (c - '0'), c = inchar();
+    return x * p;
+}
+
+pa A[mx + 5];
+
+signed main(){
+
+    #define name "Sherwin"
+    if (fopen(name".INP", "r")){
+        freopen(name".INP", "r", stdin);
+        freopen(name".OUT", "w", stdout);
+    }
+
+    #define name "temp-loops"
+    if (fopen(name".INP", "r")){
+        freopen(name".INP", "r", stdin);
+        freopen(name".OUT", "w", stdout);
+    }
+
+    ios_base:: sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int n;
+    n = Read_int();
+    fu(i, 1, n) {
+        A[i].fi = Read_int();
+        A[i].se = i;
+    }
+    sort(A + 1, A + n + 1, greater<pa>());
+    int Xmax = 0, Ymin = inf;
+    int j = 1;
+    int ans = 0;
+    fu(i, 1, n){
+        while (j <= n && A[i].fi <= A[j].fi){
+            Xmax = max(Xmax, A[j].se);
+            Ymin = min(Ymin, A[j].se);
+            ++j;
+        }
+        ans = max(ans, A[i].fi*(max(Xmax - A[i].se, A[i].se - Ymin) - 1));
+    }
+    cout << ans;
+}
+/* 
+
+  /\__/\
+ (=^.^= )
+ (") (")_/
+
+~~~-Sherwin-~~~
+
+*/
